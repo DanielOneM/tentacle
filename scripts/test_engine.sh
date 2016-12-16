90a57c7d6ae8349d3d7e3c958beedcec32927696
Daniel Enache <daniel.enache@onem.com>
2016-12-16 18:16:56 +0200

    minor correction to sleep timing after aerospike container startup

--- scripts/test_engine.sh
+++ scripts/test_engine.sh
@@ -6,7 +6,7 @@ if [ -z "$RUNNING" ]; then
 	docker run -tid --name aerospike -p 3000:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 aerospike/aerospike-server
 	docker run -tid --name rmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
 	echo 'waiting for aerospike start-up.'
-    sleep 5
+    sleep 10
 else
 	echo 'rmq and aerospike containers already running.'
 fi
